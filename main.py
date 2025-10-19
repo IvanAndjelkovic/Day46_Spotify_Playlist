37414d2d2fc87184308707be4b1d8734efd586cf
Ivan Andjelkovic <83224969+IvanAndjelkovic@users.noreply.github.com>
2025-10-19 14:32:11 +0000

    Adding requests on the web site

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
+import requests 
 
 
-input_date = input("What Year you would like to travel in format: YYYY-MM-DD")
+
+input_date = input("What Year you would like to travel in format: YYYY-MM-DD:\n")
 print(input_date)
+
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36"}
+
+url = "https://www.billboard.com/charts/hot-100/"+input_date
+
+response = requests.get(url=url, headers = headers)
+
+print(response.json())
