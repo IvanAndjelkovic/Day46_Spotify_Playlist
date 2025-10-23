31fa31d7284e235e3e1f6b85164991cc1caa3aae
Ivan Andjelkovic <83224969+IvanAndjelkovic@users.noreply.github.com>
2025-10-23 05:39:34 +0000

    Creating a list of 100 song titles from Billboard hot 100 chart on specific date and saving it in the list

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 
 
 input_date = input("What Year you would like to travel in format: YYYY-MM-DD:\n")
-print(input_date)
+
 
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36"}
 
@@ -12,10 +12,19 @@ url = "https://www.billboard.com/charts/hot-100/"+input_date
 
 response = requests.get(url=url, headers = headers)
 
-print(response.text)
+
 
 soup = BeautifulSoup(response.text, "html.parser")
 
-chart_results = soup.find_all("li", class_="o-chart-results-list__item")
-for item in chart_results:
-    print(item.find("h3", id="Title-of-a-story").get_text(strip=True)).strip()
+
+
+list_100 = []
+tags= soup.select("li.o-chart-results-list__item h3.c-title")
+for tag in tags:
+    song_title = tag.getText().strip()
+    list_100.append(song_title)
+
+print(list_100)
+
+
+
