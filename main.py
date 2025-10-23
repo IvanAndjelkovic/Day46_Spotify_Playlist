32490e3afb905daed754bc13c304dd0c07f620a6
Ivan Andjelkovic <83224969+IvanAndjelkovic@users.noreply.github.com>
2025-10-23 15:19:05 +0000

    Connecting to the spotify API via spotipy and printing out track names from a search query.

--- main.py
+++ main.py
@@ -1,30 +1,46 @@
 import requests 
 from bs4 import BeautifulSoup
+import dotenv
+import os
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+
+CLIENT_ID = dotenv.get_key(".env", "CLIENT_ID") 
+CLIENT_SECRET = dotenv.get_key(".env", "CLIENT_SECRET")
+REDIRECT_URI = "https://example.com"
 
 
 
-input_date = input("What Year you would like to travel in format: YYYY-MM-DD:\n")
+# input_date = input("What Year you would like to travel in format: YYYY-MM-DD:\n")
 
 
-headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36"}
+# headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36"}
 
-url = "https://www.billboard.com/charts/hot-100/"+input_date
+# url = "https://www.billboard.com/charts/hot-100/"+input_date
 
-response = requests.get(url=url, headers = headers)
-
-
-
-soup = BeautifulSoup(response.text, "html.parser")
+# response = requests.get(url=url, headers = headers)
 
 
 
-list_100 = []
-tags= soup.select("li.o-chart-results-list__item h3.c-title")
-for tag in tags:
-    song_title = tag.getText().strip()
-    list_100.append(song_title)
-
-print(list_100)
+# soup = BeautifulSoup(response.text, "html.parser")
 
 
+
+# list_100 = []
+# tags= soup.select("li.o-chart-results-list__item h3.c-title")
+# for tag in tags:
+#     song_title = tag.getText().strip()
+#     list_100.append(song_title)
+
+# print(list_100)
+
+
+
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=CLIENT_ID,
+                                                           client_secret=CLIENT_SECRET))
+
+results = sp.search(q='weezer', limit=20)
+for idx, track in enumerate(results['tracks']['items']):
+    print(idx, track['name'])
 
