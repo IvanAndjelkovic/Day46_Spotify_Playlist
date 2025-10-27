e04f201e6e968378efac54067995034419ff8a02
Ivan Andjelkovic <83224969+IvanAndjelkovic@users.noreply.github.com>
2025-10-27 21:05:22 +0000

    update the  search

--- main.py
+++ main.py
@@ -13,28 +13,28 @@ REDIRECT_URI = "https://example.com"
 scope = "playlist-modify-private"
 
 
-# input_date = input("What Year you would like to travel in format: YYYY-MM-DD:\n")
+input_date = input("What Year you would like to travel in format: YYYY-MM-DD:\n")
 
 
-# headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36"}
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36"}
 
-# url = "https://www.billboard.com/charts/hot-100/"+input_date
+url = "https://www.billboard.com/charts/hot-100/"+input_date
 
-# response = requests.get(url=url, headers = headers)
-
-
-
-# soup = BeautifulSoup(response.text, "html.parser")
+response = requests.get(url=url, headers = headers)
 
 
 
-# list_100 = []
-# tags= soup.select("li.o-chart-results-list__item h3.c-title")
-# for tag in tags:
-#     song_title = tag.getText().strip()
-#     list_100.append(song_title)
+soup = BeautifulSoup(response.text, "html.parser")
 
-# print(list_100)
+
+
+list_100 = []
+tags= soup.select("li.o-chart-results-list__item h3.c-title")
+for tag in tags:
+    song_title = tag.getText().strip()
+    list_100.append(song_title)
+
+print(list_100)
 
 
 
@@ -52,16 +52,16 @@ scope = "playlist-modify-private"
 #     scope=scope
 # ))
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
-    client_secret=CLIENT_SECRET,
-    redirect_uri=REDIRECT_URI,
-    scope=scope,
-    open_browser=True  # This will open the browser automatically
-))
+# sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
+#     client_secret=CLIENT_SECRET,
+#     redirect_uri=REDIRECT_URI,
+#     scope=scope,
+#     open_browser=True  # This will open the browser automatically
+# ))
 
-# Test the connection
-current_user = sp.current_user()
-print(f"Connected to Spotify as: {current_user['display_name']}")
+# # Test the connection
+# current_user = sp.current_user()
+# print(f"Connected to Spotify as: {current_user['display_name']}")
 
-current_user_id = current_user['id']
-print(f"User ID: {current_user_id}")
+# current_user_id = current_user['id']
+# print(f"User ID: {current_user_id}")
